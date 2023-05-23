4f76240051045c289987897deb38fb438962a732
khaihoang004 <130827999+khaihoang004@users.noreply.github.com>
2023-05-23 19:21:52 +0700

    Initial commit

---
+++ Danhsachsinhvien.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quanlysinhvien
+{
+    internal class Danhsachsinhvien
+    {
+        List<SinhVien> List = new List<SinhVien>();
+        internal class SinhVien
+        {
+            private bool IsNumber(string input)
+            {
+                foreach (Char c in input)
+                {
+                    if (!Char.IsDigit(c))
+                        return false;
+                }
+                return true;
+            }
+
+            public static string ChuanhoaID(ref int input)
+            {
+                int n = 6;  //So chu so cua ID
+                string temp = Convert.ToString(input);
+                for (int i = 0; i < n - Convert.ToString(input).Length; i++)
+                {
+                    temp = "0" + temp;
+                }
+                return temp;
+            }
+
+            public static string ChuanhoaID(ref string input)
+            {
+                int n = 6;  //So chu so cua ID
+                string temp = input;
+                for (int i = 0; i < n - Convert.ToString(input).Length; i++)
+                {
+                    temp = "0" + temp;
+                }
+                return temp;
+            }
+
+            public static void Chuanhoaten(ref string temp)
+            {
+                string[] ten = temp.Split(" ");
+
+            hoten:  //Nhap Ho va Ten
+                for (int k = 0; k < ten.Length; k++)
+                {
+                    for (int i = 0; i < ten[k].Length; i++)
+                    {
+
+                        if (ten[k].Length > 0)
+                        {
+                            string temp1 = ten[k];
+                            ten[k] = Convert.ToString(temp1[0]).ToUpper();
+                            for (int j = 1; j < temp1.Length; j++)
+                            {
+                                ten[k] += Convert.ToString(temp1[j]).ToLower();
+                            }
+                        }
+                    }
+                }
+                temp = "";
+                for (int i = 0; i < ten.Length; i++)
+                {
+                    temp += ten[i];
+                    if (i < ten.Length - 1 && ten[i] != "")
+                    {
+                        temp += " ";
+                    }
+                }
+            }
+
+            private string _id;
+            public string ID
+            {
+                get { return _id; }
+                set { _id = value; }
+            }
+
+            private string _name;
+            public string Name
+            {
+                get { return _name; }
+                set { _name = value; }
+            }
+
+            private string _gender;
+            public string Gender
+            {
+                get { return _gender; }
+                set { _gender = value; }
+            }
+
+            private int _age;
+            public int Age
+            {
+                get { return _age; }
+                set { _age = value; }
+            }
+
+            private double _C_sharp_point;
+            public double C_sharp_point
+            {
+                get { return _C_sharp_point; }
+                set { _C_sharp_point = value; }
+            }
+            private double _Unity_point;
+            public double Unity_point
+            {
+                get { return _Unity_point; }
+                set { _Unity_point = value; }
+            }
+            private double _3D_point;
+            public double _3Dpoint
+            {
+                get { return _3D_point; }
+                set { _3D_point = value; }
+            }
+            private double _avr_point;
+            public double Avr_point
+            {
+                get { return _avr_point; }
+                set { _avr_point = value; }
+            }
+            private string _xep_loai;
+            public string Xep_loai
+            {
+                get { return _xep_loai; }
+                set { _xep_loai = value; }
+            }
+
+
+
+
+
+
+            public void Diemtrungbinh()
+            {
+                _avr_point = (_C_sharp_point + _Unity_point + _3D_point) / 3;
+                Console.WriteLine(_avr_point);
+            }
+
+            public void Xeploai()
+            {
+                _avr_point = (_C_sharp_point + _Unity_point + _3D_point) / 3;
+                if (_avr_point >= 8)
+                {
+                    _xep_loai = "Gioi";
+                }
+                else if (_avr_point >= 6.5)
+                {
+                    _xep_loai = "Kha";
+                }
+                else if (_avr_point >= 5)
+                {
+                    _xep_loai = "Trung binh";
+                }
+                else
+                {
+                    _xep_loai = "Yeu";
+                }
+                Console.WriteLine(_xep_loai);
+            }
+            public void Nhapthongtin(int a)  //Nhap thong tin vao
+            {
+
+                _id = ChuanhoaID(ref a);
+                Console.WriteLine("Nhap thong tin hoc vien moi");
+                Console.WriteLine("-----------------------");
+                Console.Write($"Ho ten: "); _name = Console.ReadLine();
+                Chuanhoaten(ref _name);
+                do
+                {
+                    Console.Write($"Gioi tinh (Nam/Nu/Khac): "); _gender = Console.ReadLine();
+                }
+                while (_gender != "Nam" && _gender != "Nu" && _gender != "Khac");
+
+                {
+                    string temp;
+                    do
+                    {
+                        Console.Write($"Tuoi: "); temp = Console.ReadLine();
+
+                    }
+                    while (IsNumber(temp) == false);
+                    _age = Convert.ToInt32(temp);
+                }
+
+                {
+                    string temp;
+                    int temp1;
+                    do
+                    {
+                        Console.Write($"Diem C#: "); temp = Console.ReadLine();
+                        temp1 = Convert.ToInt32(temp);
+                    }
+                    while (IsNumber(temp) == false || 0 > temp1 || 10 < temp1);
+                    _C_sharp_point = Convert.ToInt32(temp);
+                }
+
+                {
+                    string temp;
+                    int temp1;
+                    do
+                    {
+                        Console.Write($"Diem Unity: "); temp = Console.ReadLine();
+                        temp1 = Convert.ToInt32(temp);
+                    }
+                    while (IsNumber(temp) == false || 0 > temp1 || 10 < temp1);
+                    _Unity_point = Convert.ToInt32(temp);
+                }
+
+                {
+                    string temp;
+                    int temp1;
+                    do
+                    {
+                        Console.Write($"Diem 3D: "); temp = Console.ReadLine();
+                        temp1 = Convert.ToInt32(temp);
+                    }
+                    while (IsNumber(temp) == false || 0 > temp1 || 10 < temp1);
+                    _3D_point = Convert.ToInt32(temp);
+                }
+
+                Diemtrungbinh();
+                Xeploai();
+            }
+
+
+            public void Show()
+            {
+                Console.WriteLine($"Thong tin hoc vien ID {_id}");
+                Console.WriteLine("-----------------------");
+                Console.Write($"Ho ten: {_name}\n");
+                Console.Write($"Gioi tinh: {_gender}\n");
+                Console.Write($"Tuoi: {_age}\n");
+                Console.Write($"Diem C#: {_C_sharp_point} \n");
+                Console.Write($"Diem Unity: {_Unity_point} \n");
+                Console.Write($"Diem 3D: {_3D_point} \n");
+                Console.Write($"Diem Trung Binh: {_avr_point} \n");
+                Console.Write($"Xep loai hoc luc: {_xep_loai} \n");
+            }
+
+
+        }
+
+
+        public static int Search_ID(List<SinhVien> List, string input)  //Tim kiem vi tri cua ID trong List
+        {
+            int index;
+            for (int i = 0; i < List.Count; i++)
+            {
+                if (List[i].ID == input)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        public static int Search_Name(List<SinhVien> List, string input)  //Tim kiem vi tri cua ID trong List
+        {
+            int index;
+            for (int i = 0; i < List.Count; i++)
+            {
+                if (List[i].Name == input)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        public static void Update_ID(List<SinhVien> List, string input)
+        {
+            int index = Search_ID(List, input);
+            List[index].Nhapthongtin(int.Parse(input));
+        }
+        public static void Delete_ID(List<SinhVien> List, string input)
+        {
+            int index = Search_ID(List, input);
+            List.Remove(List[index]);
+        }
+
+        public static void Sapxep_avr_point(ref List<SinhVien> a)
+        {
+
+
+
+
+            for (int i = 0; i < a.Count - 1; i++)
+            {
+                bool swapped = false;
+                for (int j = 0; j < a.Count - i - 1; j++)
+                {
+                    if (a[j].Avr_point > a[j + 1].Avr_point)
+                    {
+                        var temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                        swapped = true;
+                    }
+                }
+                if (!swapped)
+                {
+                    break;
+                }
+            }
+        }
+
+        public static void Hienthi_ten(List<SinhVien> List)
+        {
+            for (int i = 0; i < List.Count; i++)
+            {
+                Console.WriteLine(List[i].Name);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+
+namespace Quanlysinhvien;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        {
+            List<Danhsachsinhvien.SinhVien> List = new List<Danhsachsinhvien.SinhVien>();
+            int count = 1;
+            int n = 1;
+
+
+            while (n < 10)
+            {
+
+                switch (n)
+                {
+                    case 1:
+                        {
+
+                            Danhsachsinhvien.SinhVien a = new Danhsachsinhvien.SinhVien();
+                            Console.WriteLine("Nhap ID Sinh vien muon cap nhat thong tin: ");
+
+                            a.Nhapthongtin(count);
+                            List.Add(a);
+                            count++;
+                            Console.Clear();
+                            break;
+
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("Nhap ID Sinh vien can tim: ");
+                            string input = Console.ReadLine();
+                            int id = Convert.ToInt32(input);
+                            Console.Clear();
+                            List[id - 1].Show();
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.Clear();
+                            Console.WriteLine("- An 1 neu muon tim kiem theo ID");
+                            Console.WriteLine("- An 2 neu muon tim kiem theo ten");
+                            int a = int.Parse(Console.ReadLine());
+                            switch (a)
+                            {
+                                case 1:
+                                    {
+                                        Console.WriteLine("Nhap ID Sinh vien can tim kiem: ");
+                                        string input = Console.ReadLine();
+                                        Console.Clear();
+                                        int index = Danhsachsinhvien.Search_ID(List, input);
+                                        if (index != -1)
+                                        {
+                                            List[index].Show();
+                                        }
+                                        break;
+                                    }
+                                case 2:
+                                    {
+                                        Console.WriteLine("Nhap ten Sinh vien can tim kiem: ");
+                                        string input = Console.ReadLine();
+                                        Console.Clear();
+                                        int index = Danhsachsinhvien.Search_Name(List, input);
+                                        if (index != -1)
+                                        {
+                                            List[index].Show();
+                                        }
+                                        break;
+                                    }
+                            }
+                            break;
+                        }
+                    case 4:
+                        {
+                            Console.WriteLine("Nhap ID Sinh vien can cap nhat: ");
+                            string input = Console.ReadLine();
+                            int id = Convert.ToInt32(input);
+                            Danhsachsinhvien.Update_ID(List, input);
+                            Console.Clear();
+                            break;
+
+                        }
+                    case 5:
+                        {
+                            Console.WriteLine("Nhap ID Sinh vien can xoa: ");
+                            string input = Console.ReadLine();
+                            int id = Convert.ToInt32(input);
+                            Danhsachsinhvien.Delete_ID(List, input);
+                            Console.Clear();
+                            break;
+                        }
+                    case 6:
+                        {
+                            Console.WriteLine("Nhap ID Sinh vien can xoa: ");
+                            string input = Console.ReadLine();
+                            Danhsachsinhvien.Delete_ID(List, input);
+                            Console.Clear();
+                            break;
+                        }
+                    case 7:
+                        {
+                            Console.WriteLine($"So luong sinh vien: {List.Count}");
+                            break;
+                        }
+                    case 8:
+                        {
+                            Console.WriteLine("Sap xep theo diem trung binh: ");
+                            var a = List;
+                            for (int i = 0; i < a.Count; i++)
+                            {
+                                a.Add(List[i]);
+                            }
+                            Danhsachsinhvien.Sapxep_avr_point(ref a);
+                            Danhsachsinhvien.Hienthi_ten(a);
+                            break;
+                        }
+                    case 9:
+                        {
+                            Console.WriteLine("Hien thi ten sinh vien: ");
+                            Danhsachsinhvien.Hienthi_ten(List);
+                            break;
+                        }
+                    case 0:
+                        {
+                            Console.WriteLine("Sap xep theo ten: ");
+                            var a = List;
+                            for (int i = 0; i < a.Count; i++)
+                            {
+                                a.Add(List[i]);
+                            }
+                            Danhsachsinhvien.Sapxep_avr_point(ref a);
+                            break;
+                        }
+
+                };
+                Console.WriteLine("Ban co muon tiep tuc?");
+                Console.ReadKey();
+                Console.Clear();
+                Console.WriteLine("Chon mot hanh dong muon lam:");
+                Console.WriteLine("- An 1 neu muon dien thong tin");
+                Console.WriteLine("- An 2 neu muon xem lai thong tin");
+                Console.WriteLine("- An 3 neu tim kiem thong tin");
+                Console.WriteLine("- An 4 neu muon dung lai");
+                Console.WriteLine("- An 5 neu muon xoa 1 sinh vien");
+                Console.WriteLine("- An 6 neu muon xem so luong sinh vien");
+                Console.WriteLine("- An 7 neu tim kiem thong tin");
+                Console.WriteLine("- An 8 neu muon sap xep theo diem trung binh");
+                Console.WriteLine("- An 9 neu muon hien thi danh sach ten sinh vien");
+
+                n = int.Parse(Console.ReadLine());
+                Console.Clear();
+            }
+
+
+
+        }
+    }
+}
